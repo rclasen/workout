cb8067832226012032dfd057726e6ccbcb45f66e
Rainer Clasen <bj@zuto.de>
2012-02-23 16:12:31 +0100

    made chart settings configurable

--- lib/Workout/Chart/Delta.pm
+++ lib/Workout/Chart/Delta.pm
@@ -12,15 +12,19 @@ sub new {
 		plot_box	=> 0,
 		field		=> 'spd',
 
+		#min		=> undef,
+		#max		=> undef,
+		#tic_step	=> undef,
+		#tic_num		=> undef,
+		#tic_at		=> undef,
+		#label_fmt	=> undef,
+
 		( $a ? %$a : ()),
 
 		source		=> [],		# workouts
 	});
 
 	# TODO: support multiple fields
-	# TODO: make tics configurable
-	# TODO: make default min/max configurable
-
 
 	$self->add_scale( time	=> {
 		# bind to axis:
@@ -45,46 +49,88 @@ sub new {
 	if( $self->{field} eq 'pwr' ){
 		$self->add_scale( pwr	=> {
 			position	=> 1,
-			min		=> 0,
-			max		=> 600,
-			tic_step	=> 50,
-			label_fmt	=> '%d',
+			min		=> exists $self->{min}
+				? $self->{min} : 0,
+			max		=> exists $self->{max}
+				? $self->{max} : 600,
+			tic_step	=> exists $self->{tic_step}
+				? $self->{tic_step} : 50,
+			tic_num		=> exists $self->{tic_num}
+				? $self->{tic_num} : undef,
+			tic_at		=> exists $self->{tic_at}
+				? $self->{tic_at} : undef,
+			label_fmt	=> exists $self->{label_fmt}
+				? $self->{label_fmt} : '%d',
 			scale_label	=> 'Power (W)',
 		});
 
 	} elsif( $self->{field} eq 'spd' ){
 		$self->add_scale( spd	=> {
 			position	=> 1,
-			min		=> 0,
-			max		=> 60,
-			tic_num		=> 6,
-			label_fmt	=> '%d',
+			min		=> exists $self->{min}
+				? $self->{min} : 0,
+			max		=> exists $self->{max}
+				? $self->{max} : 60,
+			tic_step	=> exists $self->{tic_step}
+				? $self->{tic_step} : undef,
+			tic_num		=> exists $self->{tic_num}
+				? $self->{tic_num} : 6,
+			tic_at		=> exists $self->{tic_at}
+				? $self->{tic_at} : undef,
+			label_fmt	=> exists $self->{label_fmt}
+				? $self->{label_fmt} : '%d',
 			scale_label	=> 'Speed (km/h)',
 		});
 
 	} elsif( $self->{field} eq 'hr' ){
 		$self->add_scale( hr	=> {
 			position	=> 1,
-			min		=> 40,
-			max		=> 200,
-			tic_at		=> [    0,  120,  135, 145, 165, 180, 220 ],
-			label_fmt	=> [qw/ low rekom ga1  ga2  eb   sb   hai/],
+			min		=> exists $self->{min}
+				? $self->{min} : 40,
+			max		=> exists $self->{max}
+				? $self->{max} : 200,
+			tic_step	=> exists $self->{tic_step}
+				? $self->{tic_step} : undef,
+			tic_num		=> exists $self->{tic_num}
+				? $self->{tic_num} : undef,
+			tic_at		=> exists $self->{tic_at}
+				? $self->{tic_at}
+				: [    0,  120,  135, 145, 165, 180, 220 ],
+			label_fmt	=> exists $self->{label_fmt}
+				? $self->{label_fmt}
+				: [qw/ low rekom ga1  ga2  eb   sb   hai/],
 			scale_label	=> 'Heartrate (1/min)',
 		});
 
 	} elsif( $self->{field} eq 'cad' ){
 		$self->add_scale( cad	=> {
 			position	=> 1,
-			min		=> 40,
-			max		=> 200,
-			label_fmt	=> '%d',
+			min		=> exists $self->{min}
+				? $self->{min} : 40,
+			max		=> exists $self->{max}
+				? $self->{max} : 200,
+			tic_step	=> exists $self->{tic_step}
+				? $self->{tic_step} : undef,
+			tic_num		=> exists $self->{tic_num}
+				? $self->{tic_num} : undef,
+			tic_at		=> exists $self->{tic_at}
+				? $self->{tic_at} : undef,
+			label_fmt	=> exists $self->{label_fmt}
+				? $self->{label_fmt} : '%d',
 			scale_label	=> 'Cadence (1/min)',
 		});
 
 	} elsif( $self->{field} eq 'ele' ){
 		$self->add_scale( ele	=> {
 			position	=> 1,
-			label_fmt	=> '%.1f',
+			tic_step	=> exists $self->{tic_step}
+				? $self->{tic_step} : undef,
+			tic_num		=> exists $self->{tic_num}
+				? $self->{tic_num} : undef,
+			tic_at		=> exists $self->{tic_at}
+				? $self->{tic_at} : undef,
+			label_fmt	=> exists $self->{label_fmt}
+				? $self->{label_fmt} : '%.1f',
 			scale_label	=> 'Elevation (m)',
 		});
 
--- lib/Workout/Chart/Workout.pm
+++ lib/Workout/Chart/Workout.pm
@@ -16,7 +16,7 @@ our %color = (
 
 our %default = (
 	xfield	=> 'time',
-	fields	=> [qw/ ele spd cad hr pwr /],
+	fields	=> { map { $_ => {} } [qw/ ele spd cad hr pwr /] },
 );
 
 __PACKAGE__->mk_ro_accessors( keys %default );
@@ -35,9 +35,10 @@ sub new {
 		line_style	=> 0,		# per workout line_style
 	});
 
+	my $t = $self->fields;
+
 	# TODO: marker
 	# TODO: make tics configurable
-	# TODO: make default min/max configurable
 	# TODO: tooltips with values under mouse cursor
 	# TODO: fields total/last n sec: npwr, spd_av, pwr_av
 	# TODO: sum fields: odo, work
@@ -76,80 +77,143 @@ sub new {
 
 	$self->add_scale( pwr	=> {
 		position	=> 1,
-		min		=> 0,
-		max		=> 600,
-		#tic_step	=> 25,
-		label_fmt	=> '%d',
+		min		=> exists $t->{pwr}{min}
+			? $t->{pwr}{min} : 0,
+		max		=> exists $t->{pwr}{max}
+			? $t->{pwr}{max} : 600,
+		tic_step	=> exists $t->{pwr}{tic_step}
+			? $t->{pwr}{tic_step} : 50,
+		tic_num		=> exists $t->{pwr}{tic_num}
+			? $t->{pwr}{tic_num} : undef,
+		tic_at		=> exists $t->{pwr}{tic_at}
+			? $t->{pwr}{tic_at} : undef,
+		label_fmt	=> exists $t->{pwr}{label_fmt}
+			? $t->{pwr}{label_fmt} : '%d',
 		label_fg	=> $self->{color}{pwr},
 		scale_label_fg	=> $self->{color}{pwr},
 		scale_label	=> 'Power (W)',
-	}) if grep { /^pwr$/ } @{$self->fields};
+	}) if $t->{pwr};
 
 	$self->add_scale( spd	=> {
 		position	=> 1,
-		min		=> 0,
-		max		=> 60,
-		tic_step	=> 5,
-		label_fmt	=> '%d',
+		min		=> exists $t->{spd}{min}
+			? $t->{spd}{min} : 0,
+		max		=> exists $t->{spd}{max}
+			? $t->{spd}{max} : 60,
+		tic_step	=> exists $t->{spd}{tic_step}
+			? $t->{spd}{tic_step} : 5,
+		tic_num		=> exists $t->{spd}{tic_num}
+			? $t->{spd}{tic_num} : undef,
+		tic_at		=> exists $t->{spd}{tic_at}
+			? $t->{spd}{tic_at} : undef,
+		label_fmt	=> exists $t->{spd}{label_fmt}
+			? $t->{spd}{label_fmt} : '%d',
 		label_fg	=> $self->{color}{spd},
 		scale_label_fg	=> $self->{color}{spd},
 		scale_label	=> 'Speed (km/h)',
-	}) if grep { /^spd$/ } @{$self->fields};
+	}) if $t->{spd};
 
 	$self->add_scale( grad	=> {
 		position	=> 1,
-		min		=> 0,
-		max		=> 20,
-		tic_step	=> 2,
-		label_fmt	=> '%d',
+		min		=> exists $t->{grad}{min}
+			? $t->{grad}{min} : 0,
+		max		=> exists $t->{grad}{max}
+			? $t->{grad}{max} : 20,
+		tic_step	=> exists $t->{grad}{tic_step}
+			? $t->{grad}{tic_step} : 2,
+		tic_num		=> exists $t->{grad}{tic_num}
+			? $t->{grad}{tic_num} : undef,
+		tic_at		=> exists $t->{grad}{tic_at}
+			? $t->{grad}{tic_at} : undef,
+		label_fmt	=> exists $t->{grad}{label_fmt}
+			? $t->{grad}{label_fmt} : '%d',
 		label_fg	=> $self->{color}{grad},
 		scale_label_fg	=> $self->{color}{grad},
 		scale_label	=> 'Gradient (%)',
-	}) if grep { /^grad$/ } @{$self->fields};
+	}) if $t->{grad};
 
 	# right axis
 
 	$self->add_scale( hr	=> {
 		position	=> 2,
-		min		=> 40,
-		max		=> 200,
+		min		=> exists $t->{hr}{min}
+			? $t->{hr}{min} : 40,
+		max		=> exists $t->{hr}{max}
+			? $t->{hr}{max} : 200,
+		tic_step	=> exists $t->{hr}{tic_step}
+			? $t->{hr}{tic_step} : undef,
+		tic_num		=> exists $t->{hr}{tic_num}
+			? $t->{hr}{tic_num} : undef,
+		tic_at		=> exists $t->{hr}{tic_at}
+			? $t->{hr}{tic_at} : undef,
+		label_fmt	=> exists $t->{hr}{label_fmt}
+			? $t->{hr}{label_fmt} : '%d',
 		#tic_at		=> [    0,  120,  135, 145, 165, 180, 220 ],
 		#label_fmt	=> [qw/ low rekom ga1  ga2  eb   sb   hai/],
-		#tic_step	=> 10,
-		label_fmt	=> '%d',
 		label_fg	=> $self->{color}{hr},
 		scale_label_fg	=> $self->{color}{hr},
 		scale_label	=> 'Heartrate (1/min)',
-	}) if grep { /^hr$/ } @{$self->fields};
+	}) if $t->{hr};
 
 	$self->add_scale( cad	=> {
 		position	=> 2,
-		min		=> 40,
-		max		=> 200,
-		tic_step	=> 10,
-		label_fmt	=> '%d',
+		min		=> exists $t->{cad}{min}
+			? $t->{cad}{min} : 40,
+		max		=> exists $t->{cad}{max}
+			? $t->{cad}{max} : 200,
+		tic_step	=> exists $t->{cad}{tic_step}
+			? $t->{cad}{tic_step} : 10,
+		tic_num		=> exists $t->{cad}{tic_num}
+			? $t->{cad}{tic_num} : undef,
+		tic_at		=> exists $t->{cad}{tic_at}
+			? $t->{cad}{tic_at} : undef,
+		label_fmt	=> exists $t->{cad}{label_fmt}
+			? $t->{cad}{label_fmt} : '%d',
 		label_fg	=> $self->{color}{cad},
 		scale_label_fg	=> $self->{color}{cad},
 		scale_label	=> 'Cadence (1/min)',
-	}) if grep { /^cad$/ } @{$self->fields};
+	}) if $t->{cad};
 
 	# hidden vertical axis
 	# TODO: change ele color based on slope in %
 	$self->add_scale( ele	=> {
 		position	=> undef,
-		label_fmt	=> '%.1f',
+		min		=> exists $t->{ele}{min}
+			? $t->{ele}{min} : undef,
+		max		=> exists $t->{ele}{max}
+			? $t->{ele}{max} : undef,
+		tic_step	=> exists $t->{ele}{tic_step}
+			? $t->{ele}{tic_step} : undef,
+		tic_num		=> exists $t->{ele}{tic_num}
+			? $t->{ele}{tic_num} : undef,
+		tic_at		=> exists $t->{ele}{tic_at}
+			? $t->{ele}{tic_at} : undef,
+		label_fmt	=> exists $t->{ele}{label_fmt}
+			? $t->{ele}{label_fmt} : '%.1f',
 		label_fg	=> $self->{color}{ele},
 		scale_label_fg	=> $self->{color}{ele},
 		scale_label	=> 'Elevation (m)',
-	}) if grep { /^ele$/ } @{$self->fields};
+	}) if $t->{ele};
 
 	# TODO: fields: temp, torque, deconv, vspd, grad, accel
-	foreach my $f ( @{ $self->fields} ){
+	foreach my $f ( keys %$t ){
 		next if $f =~ /^(?:ele|spd|hr|cad|pwr|grad)$/;
 		print STDERR "adding non-default scale: $f\n";
 
 		$self->add_scale( $f	=> {
 			scale_label	=> $f,
+			min		=> exists $t->{$f}{min}
+				? $t->{$f}{min} : undef,
+			max		=> exists $t->{$f}{max}
+				? $t->{$f}{max} : undef,
+			tic_step	=> exists $t->{$f}{tic_step}
+				? $t->{$f}{tic_step} : undef,
+			tic_num		=> exists $t->{$f}{tic_num}
+				? $t->{$f}{tic_num} : undef,
+			tic_at		=> exists $t->{$f}{tic_at}
+				? $t->{$f}{tic_at} : undef,
+			label_fmt	=> exists $t->{$f}{label_fmt}
+				? $t->{$f}{label_fmt} : undef,
 		});
 	};
 	$self;
@@ -158,8 +222,10 @@ sub new {
 sub add_workout {
 	my( $self, $wk, $fields ) = @_;
 
-	$fields ||= $self->fields;
-	my $s = Workout::Chart::Source->new( $wk, $fields );
+	my $t = $fields
+		? { map { $_ => {} } @$fields }
+		: $self->fields;
+	my $s = Workout::Chart::Source->new( $wk, [ keys %$t ] );
 
 	my $sourcecount = @{$self->{source}};
 	my $suffix = $sourcecount ? " $sourcecount" : '';
@@ -174,7 +240,7 @@ sub add_workout {
 		#xcol	=> 'time',
 		ycol	=> 'ele',
 		color	=> $self->{color}{ele},
-	}) if grep { /^ele$/ } @$fields;
+	}) if $t->{ele};
 
 	# spd
 	$self->add_plot({
@@ -183,7 +249,7 @@ sub add_workout {
 		source	=> $s,
 		color	=> $self->{color}{spd},
 		line_style	=> $self->{line_style},
-	}) if grep { /^spd$/ } @$fields;
+	}) if $t->{spd};
 
 	# grad
 	$self->add_plot({
@@ -192,7 +258,7 @@ sub add_workout {
 		source	=> $s,
 		color	=> $self->{color}{grad},
 		line_style	=> $self->{line_style},
-	}) if grep { /^grad$/ } @$fields;
+	}) if $t->{grad};
 
 	# cad
 	$self->add_plot({
@@ -201,7 +267,7 @@ sub add_workout {
 		source	=> $s,
 		color	=> $self->{color}{cad},
 		line_style	=> $self->{line_style},
-	}) if grep { /^cad$/ } @$fields;
+	}) if $t->{cad};
 
 	# hr
 	$self->add_plot({
@@ -211,7 +277,7 @@ sub add_workout {
 		source	=> $s,
 		color	=> $self->{color}{hr},
 		line_style	=> $self->{line_style},
-	}) if grep { /^hr$/ } @$fields;
+	}) if $t->{hr};
 
 	# pwr
 	$self->add_plot({
@@ -220,9 +286,9 @@ sub add_workout {
 		source	=> $s,
 		color	=> $self->{color}{pwr},
 		line_style	=> $self->{line_style},
-	}) if grep { /^pwr$/ } @$fields;
+	}) if $t->{pwr};
 
-	foreach my $f ( @{ $self->fields} ){
+	foreach my $f ( keys %$t  ){
 		next if $f =~ /^(?:ele|spd|hr|cad|pwr|grad)$/;
 		print STDERR "adding non-default plot: $f\n";
 
