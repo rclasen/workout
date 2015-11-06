fa20605ea50e7b7a79ba47f3bcc0cc5507ba6078
Rainer Clasen <bj@zuto.de>
2015-11-06 16:54:26 +0100

    constants: allow inlining

--- lib/Workout/Constant.pm
+++ lib/Workout/Constant.pm
@@ -29,22 +29,22 @@ BEGIN {
 	Exporter::export_ok_tags('all');
 }
 
-sub PI { return 3.14159265 }
-sub EULER { return 2.718281 }	# ()		Eulersche Zahl
+sub PI() { 3.14159265 }
+sub EULER() { 2.718281 }	# ()		Eulersche Zahl
 
-sub NSEC { return 1000000000 }	# (nsec)	factor / 1 sec = NSEC nanonsec
-sub HOUR { return 3600 }	# (sec)		factor / 1 hour = HOUR seconds
-sub KELVIN { return 273.15 }	# (C)		delta / 0 kelvin = KELVIN Celsius
-sub KCAL { return 4186.8 }	# (J)		factor / 1 kcal = KCAL Joule
-sub KMH { return 3.6 }		# (m/s)		factor / 1 km/h = KMH meter/sec
-sub FEET { return 0.3048 }	# (m)		factor / 1 feet = FEET meter
-sub YARD { return 0.9144 }	# (m)		factor / 1 yard = YARD meter
-sub MILE { return 1609.344 }	# (m)		factor / 1 mile = MILE meter
-sub MPH { return 0.44704 }	# (m/s)		factor / 1 mile/h = MPH m/sec
+sub NSEC() { 1000000000 }	# (nsec)	factor / 1 sec = NSEC nanonsec
+sub HOUR() { 3600 }	# (sec)		factor / 1 hour = HOUR seconds
+sub KELVIN() { 273.15 }	# (C)		delta / 0 kelvin = KELVIN Celsius
+sub KCAL() { 4186.8 }	# (J)		factor / 1 kcal = KCAL Joule
+sub KMH() { 3.6 }		# (m/s)		factor / 1 km/h = KMH meter/sec
+sub FEET() { 0.3048 }	# (m)		factor / 1 feet = FEET meter
+sub YARD() { 0.9144 }	# (m)		factor / 1 yard = YARD meter
+sub MILE() { 1609.344 }	# (m)		factor / 1 mile = MILE meter
+sub MPH() { 0.44704 }	# (m/s)		factor / 1 mile/h = MPH m/sec
 
-sub GRAVITY { return 9.81 }	# (m/s²)	Erdbeschleunigung
-sub RHO_0 { return 1.293 }	# (kg/m³)	Luftdichte auf Meereshöhe bei 0° Celsius
-sub P_0 { return 101325 }	# (Pa = kg/(ms²)) Luftdruck auf Meereshöhe bei 0° Celsius
+sub GRAVITY() { 9.81 }	# (m/s²)	Erdbeschleunigung
+sub RHO_0() { 1.293 }	# (kg/m³)	Luftdichte auf Meereshöhe bei 0° Celsius
+sub P_0() { 101325 }	# (Pa = kg/(ms²)) Luftdruck auf Meereshöhe bei 0° Celsius
 
 
 1;
